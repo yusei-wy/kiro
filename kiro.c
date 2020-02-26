52ec97d6cf56c01c1cc48d100a22c2a0df33975c
yusei-wy <yusei.kasa@gmail.com>
2020-02-26 19:07:32 +0900

    Add Reposition the cursor

--- kiro.c
+++ kiro.c
@@ -54,7 +54,10 @@ void enableRawMode() {
 }
 
 // --- output ---
-void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
 // --- input ---
 
